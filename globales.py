1955a1e88340264d5591140d51603ffac9c0e46d
Leandro Nieto <nleandro0juarg@gmail.com>
2024-11-15 00:00:20 -0300

    Los pedidos se conectan a la base de datos

--- globales.py
+++ globales.py
@@ -1,3 +1,5 @@
+from pymongo import MongoClient
+from pymongo.database import Database
 from clases.clases_usuarios import ListaUsuarios, Usuario
 from clases.clases_pedidos import ListaPedidos
 
@@ -7,6 +9,18 @@ g_ListaPedidos = ListaPedidos()
 """Objeto global ListaPedidos"""
 g_UsuarioActual = Usuario.Vacio()
 """Usuario actual"""
+
+def conectarDB() -> Database:
+    # Reemplaza <username>, <password> y <cluster-url> con tus credenciales y URL del clúster
+    client = MongoClient("mongodb+srv://41653328:7cptheLz4s2LlvsN@clusterinso2g6.rqpj2.mongodb.net/?retryWrites=true&w=majority&appName=ClusterINSO2G6")
+
+    # Selecciona la base de datos
+    db = client["inso2_SIGEAD"]
+
+    return db
+
+g_MongoDBClient = conectarDB()
+"""Conexion a la base de datos"""
 
 def setUsuarioActual(usuario: Usuario):
     global g_UsuarioActual
